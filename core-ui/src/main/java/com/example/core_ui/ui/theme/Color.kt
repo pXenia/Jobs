d118a303cf437c7f70e08baa3b279cd60ea12f21
Xenia_Popova <p961083@gmail.com>
2025-03-08 01:42:53 +0300

    Initial commit

---
+++ core-ui/src/main/java/com/example/core_ui/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.core_ui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val Grey1 = Color(0xFF222325)
+val Grey2 = Color(0xFF313234)
+val Grey3 = Color(0xFF858688)
+val Grey4 = Color(0xFF9F9F9F)
+val Grey5 = Color(0xFFD8DBDB)
+val White = Color(0xFFFFFFFF)
+
+val Blue = Color(0xFF2B7EFE)
+val DarkBlue = Color(0xFF00427D)
+val Green = Color(0xFF4CB24E)
+val DarkGreen = Color(0xFF015905)
+val Red = Color(0xFFFF0000)
+
+
+val Shadows = Color(0x900C0C0C)
